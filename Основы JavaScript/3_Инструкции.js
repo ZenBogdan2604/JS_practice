b0e375108aaa5bc87e90b81fda13797b0afefd96
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-07-19 18:53:58 +0300

    Create 3_Инструкции.js

---
+++ Основы JavaScript/3_Инструкции.js
@@ -0,0 +1,6 @@
+/*Задание
+Выведите на экран друг за другом три имени: Robert, Stannis, Renly. В результате на экране должно отобразиться:*/
+
+console.log('Robert');
+console.log('Stannis');
+console.log('Renly');
