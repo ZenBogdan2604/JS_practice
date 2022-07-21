45d8e575fbe769a68bd71289eae3cc970ae11c2b
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-07-21 13:28:53 +0300

    Create 17_Конкатенация.js

---
+++ Строки/17_Конкатенация.js
@@ -0,0 +1,7 @@
+/*Задание
+Выведите на экран
+
+Winter came for the House of Frey.
+используя конкатенацию слов.*/
+
+console.log('Winter ' + 'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.');
